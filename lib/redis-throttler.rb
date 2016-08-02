274b525d1f82b155d998fb2e735c5ed41a78dff0
Evan Surdam <es@cosi.io>
2016-08-02 01:58:53 -0700

    fix configurator location

--- lib/redis-throttler.rb
+++ lib/redis-throttler.rb
@@ -1,5 +1,5 @@
 require 'redis'
-require 'helpers/configuration'
+require 'redis-throttler/configuration'
 require 'redis-throttler/model'
 require 'redis-throttler/base'
 
@@ -10,7 +10,7 @@ module RedisThrottler
   define_setting :default_limit, 5
   define_setting :default_threshold, 600
 
-  define_setting :redis, Redis.new(host: 'localhost', port: 6379)
+  define_setting :redis, Redis.new(host: '192.168.99.100', port: 32772)
 
   def self.included(base)
     base.include(RedisThrottler::Model)
